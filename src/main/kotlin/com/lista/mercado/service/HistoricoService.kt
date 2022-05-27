b0ebd340b9b860c53ebc5115a5863fe33972b50f
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-27 15:35:54 -0300

    Add Historico Service

--- src/main/kotlin/com/lista/mercado/service/HistoricoService.kt
+++ src/main/kotlin/com/lista/mercado/service/HistoricoService.kt
@@ -2,7 +2,9 @@ package com.lista.mercado.service
 
 import com.lista.mercado.model.Historico
 import com.lista.mercado.model.Item
+import org.springframework.stereotype.Service
 
+@Service
 class HistoricoService(private var historicos: List<Item> = ArrayList()) {
     fun listarHistorico(): List<Item> {
         return historicos
